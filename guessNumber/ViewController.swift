424b432df4427b60bda3af3b652f623cbeb3afac
Bryan Kuo <bryan0011@gmail.com>
2021-09-19 20:47:27 +0800

    final version

--- guessNumber/ViewController.swift
+++ guessNumber/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var guessTime: UILabel!
     @IBOutlet weak var playAgain: UIButton!
     @IBOutlet weak var play: UIButton!
+    @IBOutlet weak var chickenImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
         playAgain.isHidden = true
+        chickenImage.image = UIImage(named: "4")
+        
     
     }
     
@@ -31,24 +34,22 @@ class ViewController: UIViewController {
     var guessTimes = 6
     
     
-    func gameReset() {
-        
-        lowNumber = 0
-        highNumber = 50
-        guessTimes = 6
-        warning.isHidden = true
-        guessNumberText.text = ""
-        computerNumber = Int.random(in: 0...50)
-        hint.isHidden = false
-        hint.text = "數字介於\(lowNumber)~\(highNumber)之間"
-        play.isHidden = false
-        guessTime.isHidden = false
-        playAgain.isHidden = true
-        guessTime.text = "還能猜\(guessTimes)次"
-    }
+    let animation = CABasicAnimation(keyPath: "backgroundColor")
     
     
     @IBAction func playGame(_ sender: UIButton) {
+        
+        warning.isHidden = true
+        
+        animation.fromValue = UIColor.red.cgColor
+        animation.toValue = UIColor.orange.cgColor
+        animation.duration = 0.5
+        animation.repeatCount = Float.infinity
+        animation.autoreverses = true
+        
+        warning.layer.add(animation, forKey: nil)
+        
+        print(computerNumber)
         
         guessTimes -= 1
         guessTime.text = "還能猜\(guessTimes)次"
@@ -63,24 +64,35 @@ class ViewController: UIViewController {
                     guessTime.isHidden = true
                     warning.isHidden = false
                     warning.text = " 6次已經用完了，再試試下一次的運氣吧~ "
+                    chickenImage.image = UIImage(named: "2")
                     
                 }
                 
                 if guessNumberInt == computerNumber {
                     warning.isHidden = false
                     hint.isHidden = true
-                    warning.text = "太棒了，您猜對了！！！"
+                    
                     playAgain.isHidden = false
                     play.isHidden = true
                     hint.isHidden = true
                     guessTime.isHidden = true
+                    chickenImage.image = UIImage(named: "3")
+                    
+                    let gradient = CAGradientLayer()
+                    gradient.frame = warning.bounds
+                    
+                    warning.text = "太棒了，您猜對了！！！"
+
+                    
                     
                 } else if guessNumberInt < computerNumber {
                     lowNumber = guessNumberInt
                     hint.text = "數字介於\(lowNumber)~\(highNumber)之間"
+                    guessNumberText.text = ""
                 } else if guessNumberInt > computerNumber {
                     highNumber = guessNumberInt
                     hint.text = "數字介於\(lowNumber)~\(highNumber)之間"
+                    guessNumberText.text = ""
                 } else {
                 warning.isHidden = false
                 warning.text = "請輸入數字！！！"
@@ -89,6 +101,9 @@ class ViewController: UIViewController {
             } else {
                 warning.isHidden = false
                 warning.text = "請輸入0~50的數字！"
+                guessNumberText.text = ""
+    
+                  
                 
             }
             
@@ -97,8 +112,20 @@ class ViewController: UIViewController {
     }
      
     @IBAction func playAgain(_ sender: Any) {
-      gameReset()
         
+        lowNumber = 0
+        highNumber = 50
+        guessTimes = 6
+        warning.isHidden = true
+        guessNumberText.text = ""
+        computerNumber = Int.random(in: 0...50)
+        hint.isHidden = false
+        hint.text = "數字介於\(lowNumber)~\(highNumber)之間"
+        play.isHidden = false
+        guessTime.isHidden = false
+        playAgain.isHidden = true
+        guessTime.text = "還能猜\(guessTimes)次"
+        chickenImage.image = UIImage(named: "4")
     }
     
 }
